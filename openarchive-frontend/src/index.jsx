64ccce88ecfa5ca2795f9cc852465b2b77d16439
DrPepper05 <bogalima6@gmail.com>
2025-05-31 02:49:52 +0300

    fixed react-native in react

--- openarchive-frontend/src/index.jsx
+++ openarchive-frontend/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Assuming you have TailwindCSS setup here
-import App from './App';
+import App from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
